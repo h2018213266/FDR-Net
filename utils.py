1643b6edac04bb342cb09cd7afb943eca412784f
h2018213266 <84175241+h2018213266@users.noreply.github.com>
2024-09-11 19:29:42 +0800

    utils.py

--- utils.py
+++ utils.py
@@ -9,23 +9,10 @@ from PIL import Image
 import copy
 
 
-class BinaryDiceLoss(nn.Module):
-    """Dice loss of binary class
-    Args:
-        smooth: A float number to smooth loss, and avoid NaN error, default: 1
-        p: Denominator value: \sum{x^p} + \sum{y^p}, default: 2
-        predict: A tensor of shape [N, *]
-        target: A tensor of shape same with predict
-        reduction: Reduction method to apply, return mean over batch if 'mean',
-            return sum if 'sum', return a tensor of shape [N,] if 'none'
-    Returns:
-        Loss tensor according to arg reduction
-    Raise:
-        Exception if unexpected reduction
-    """
+class Loss1(nn.Module):
 
     def __init__(self, smooth=1, p=2, reduction='mean'):
-        super(BinaryDiceLoss, self).__init__()
+        super(Loss1, self).__init__()
         self.smooth = smooth
         self.p = p
         self.reduction = reduction
@@ -50,9 +37,9 @@ class BinaryDiceLoss(nn.Module):
             raise Exception('Unexpected reduction {}'.format(self.reduction))
 
 
-class DiceLoss(nn.Module):
+class Loss2(nn.Module):
     def __init__(self, n_classes):
-        super(DiceLoss, self).__init__()
+        super(Loss2, self).__init__()
         self.n_classes = n_classes
 
     def _one_hot_encoder(self, input_tensor):
@@ -88,131 +75,3 @@ class DiceLoss(nn.Module):
             class_wise_dice.append(1.0 - dice.item())
             loss += dice * weight[i]
         return loss / self.n_classes
-
-
-def calculate_metric_percase(pred, gt):
-    pred[pred > 0] = 1
-    gt[gt > 0] = 1
-    if pred.sum() > 0 and gt.sum()>0:
-        dice = metric.binary.dc(pred, gt)
-        hd95 = metric.binary.hd95(pred, gt)
-        return dice, hd95
-    elif pred.sum() > 0 and gt.sum()==0:
-        return 1, 0
-    else:
-        return 0, 0
-
-# 1
-def test_single_volume(image, label, net, classes, patch_size=[256, 256], test_save_path=None, case=None, z_spacing=1):
-    image, label = image.squeeze(0).cpu().detach().numpy(), label.squeeze(0).cpu().detach().numpy()
-    _, x, y = image.shape
-
-    # 缩放图像符合网络输入大小224x224
-    if x != patch_size[0] or y != patch_size[1]:
-        image = zoom(image, (1, patch_size[0] / x, patch_size[1] / y), order=3)
-    input = torch.from_numpy(image).unsqueeze(0).float().cuda()
-    net.eval()
-    with torch.no_grad():
-        out = torch.argmax(torch.softmax(net(input), dim=1), dim=1).squeeze(0)
-        out = out.cpu().detach().numpy()
-        # 缩放预测结果图像同原始图像大小
-        if x != patch_size[0] or y != patch_size[1]:
-            prediction = zoom(out, (x / patch_size[0], y / patch_size[1]), order=0)
-        else:
-            prediction = out
-    metric_list = []
-    for i in range(1, classes):
-        metric_list.append(calculate_metric_percase(prediction == i, label == i))
-
-    if test_save_path is not None:
-        #保存预测结果
-        # prediction = Image.fromarray(np.uint8(prediction)).convert('L')
-        # prediction.save(test_save_path + '/' + case + '.png')
-
-        a1 = copy.deepcopy(prediction)
-        a2 = copy.deepcopy(prediction)
-        a3 = copy.deepcopy(prediction)
-        # r通道
-        a1[a1 == 1] = 0
-        # g通道
-        a2[a2 == 1] = 255
-        # b通道
-        a3[a3 == 1] = 0
-        a1 = Image.fromarray(np.uint8(a1)).convert('L')
-        a2 = Image.fromarray(np.uint8(a2)).convert('L')
-        a3 = Image.fromarray(np.uint8(a3)).convert('L')
-        prediction = Image.merge('RGB', [a1, a2, a3])
-        prediction.save(test_save_path + '/' + case + '.tif')
-
-    return metric_list
-
-
-def test_single_volume1(image, label, net, classes, patch_size=[256, 256], test_save_path=None, case=None, z_spacing=1):
-    image, label = image.squeeze(0).cpu().detach().numpy(), label.squeeze(0).cpu().detach().numpy()
-    _, x, y = image.shape
-
-    # 缩放图像符合网络输入大小224x224
-    if x != patch_size[0] or y != patch_size[1]:
-        image = zoom(image, (1, patch_size[0] / x, patch_size[1] / y), order=3)
-    input = torch.from_numpy(image).unsqueeze(0).float().cuda()
-    net.eval()
-    with torch.no_grad():
-        out = torch.argmax(torch.softmax(net(input), dim=1), dim=1).squeeze(0)
-        out = out.cpu().detach().numpy()
-        # 缩放预测结果图像同原始图像大小
-        if x != patch_size[0] or y != patch_size[1]:
-            prediction = zoom(out, (x / patch_size[0], y / patch_size[1]), order=0)
-        else:
-            prediction = out
-    metric_list = []
-    for i in range(1, classes):
-        metric_list.append(calculate_metric_percase(prediction == i, label == i))
-
-    if test_save_path is not None:
-        #保存预测结果
-        prediction = Image.fromarray(np.uint8(prediction)).convert('L')
-        prediction.save(test_save_path + '/' + case + '.tif')
-    return metric_list
-
-# def test_single_volume(image, label, net, classes, patch_size=[256, 256], test_save_path=None, case=None, z_spacing=1):
-#     image, label = image.squeeze(0).cpu().detach().numpy(), label.squeeze(0).cpu().detach().numpy()
-#     if len(image.shape) == 3:
-#         prediction = np.zeros_like(label)
-#         for ind in range(image.shape[0]):
-#             slice = image[ind, :, :]
-#             x, y = slice.shape[0], slice.shape[1]
-#             if x != patch_size[0] or y != patch_size[1]:
-#                 slice = zoom(slice, (patch_size[0] / x, patch_size[1] / y), order=3)  # previous using 0
-#             input = torch.from_numpy(slice).unsqueeze(0).unsqueeze(0).float().cuda()
-#             net.eval()
-#             with torch.no_grad():
-#                 outputs = net(input)
-#                 out = torch.argmax(torch.softmax(outputs, dim=1), dim=1).squeeze(0)
-#                 out = out.cpu().detach().numpy()
-#                 if x != patch_size[0] or y != patch_size[1]:
-#                     pred = zoom(out, (x / patch_size[0], y / patch_size[1]), order=0)
-#                 else:
-#                     pred = out
-#                 prediction[ind] = pred
-#     else:
-#         input = torch.from_numpy(image).unsqueeze(
-#             0).unsqueeze(0).float().cuda()
-#         net.eval()
-#         with torch.no_grad():
-#             out = torch.argmax(torch.softmax(net(input), dim=1), dim=1).squeeze(0)
-#             prediction = out.cpu().detach().numpy()
-#     metric_list = []
-#     for i in range(1, classes):
-#         metric_list.append(calculate_metric_percase(prediction == i, label == i))
-#
-#     if test_save_path is not None:
-#         img_itk = sitk.GetImageFromArray(image.astype(np.float32))
-#         prd_itk = sitk.GetImageFromArray(prediction.astype(np.float32))
-#         lab_itk = sitk.GetImageFromArray(label.astype(np.float32))
-#         img_itk.SetSpacing((1, 1, z_spacing))
-#         prd_itk.SetSpacing((1, 1, z_spacing))
-#         lab_itk.SetSpacing((1, 1, z_spacing))
-#         sitk.WriteImage(prd_itk, test_save_path + '/'+case + "_pred.nii.gz")
-#         sitk.WriteImage(img_itk, test_save_path + '/'+ case + "_img.nii.gz")
-#         sitk.WriteImage(lab_itk, test_save_path + '/'+ case + "_gt.nii.gz")
-#     return metric_list
